d6b6a040610da3b11488e80977ca17efc25bfc68
athira2814 <athirad193@gmail.com>
2021-10-01 17:03:32 +0530

    null vs undefined

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
-// string
-console.log( typeof"hello world")
-console.log(typeof "d")
-
-// number
-console.log(typeof 1)
-console.log(typeof 1.2)
-console.log(typeof -1.2)
-console.log(typeof -1)
-console.log(typeof 1.298765678)
-
-// Boolean
-console.log(typeof true)
-console.log(typeof false)
+// null
+var age=null
+console.log(age)
+// undefined
+ var x
+ console.log(x)
