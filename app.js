130c59f7d60ff1ec3f6b838902fb4eccfbc0c234
athira2814 <athirad193@gmail.com>
2021-10-01 16:52:14 +0530

    primitive datatypes

---
+++ app.js
@@ -0,0 +1,14 @@
+// string
+console.log( typeof"hello world")
+console.log(typeof "d")
+
+// number
+console.log(typeof 1)
+console.log(typeof 1.2)
+console.log(typeof -1.2)
+console.log(typeof -1)
+console.log(typeof 1.298765678)
+
+// Boolean
+console.log(typeof true)
+console.log(typeof false)
