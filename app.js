7ef42e31a72c36dfa45a04b6f68aaeb427582545
athira2814 <athirad193@gmail.com>
2021-10-01 19:30:34 +0530

    operators

--- app.js
+++ app.js
@@ -1,6 +1,52 @@
-// null
-var age=null
-console.log(age)
-// undefined
- var x
- console.log(x)
+// operators
+
+// arithmatic operators
+
+var a = 2
+var b = 10
+console.log('a+b =', a+b)
+console.log('a-b =', a-b)
+console.log('a/b =', a/b)
+console.log('a*b =', a*b)
+console.log('a**b =', a**b)
+console.log('a%b =', a%b)
+
+// assignment operator
+
+console.log('b', b)
+b+=10
+console.log('b+10', b)
+b++
+console.log('b++', b)
+b-=1
+console.log('b-=10', b)
+b--
+console.log('b--', b)
+
+var c = 10
+console.log(c++)
+console.log(c)
+// comparison operators
+
+var d = 10
+var e =2
+f='10'
+console.log(d > e)
+console.log(d < e)
+console.log(d >= e)
+console.log(d <= e)
+console.log(d == e)
+console.warn(d == f)
+console.warn(d === f)
+
+// logical operators
+
+console.log('true && true', true && true)
+console.log('false && true', false && true)
+console.log('true && false', true && false)
+console.log('false && false', false && false)
+
+console.log('true || true', true || true)
+console.log('false || true', false || true)
+console.log('true || false', true || false)
+console.log('false || false', false || false)
