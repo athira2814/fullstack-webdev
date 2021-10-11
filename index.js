32125a3e88e3be4b8a5000e06c0389b949621b76
athira2814 <athirad193@gmail.com>
2021-10-11 21:41:11 +0530

    object methods

--- index.js
+++ index.js
@@ -1,53 +1,4 @@
-// eg 1
-let car = {
-    name : "class",
-    manufacturer :"mercedes"
-}
-console.log(car.name)
-console.log(car.manufacturer)
-
-// eg 2
-let car = {
-    name : "class",
-    manufacturer :"mercedes",
-    print : function(){
-        console.log("bike")
-    }
-}
-car.print()
-
-// eg 3
-let car = {
-    name : "class",
-    manufacturer :"mercedes",
-    print : function(){
-        console.log(car.name)
-    }
-}
-car.print()
-
-// eg 4
-let car = {
-    name : "class",
-    manufacturer :"mercedes",
-    print : function(){
-        console.log(this)
-        console.log(this.name)
-    }
-}
-car.print()
-
-// eg 5
-let car = {
-    name : "class",
-    manufacturer :"mercedes",
-    print : function(){
-        console.log(this.name + ' was manufactured by ' + this.manufacturer)
-    }
-}
-car.print()
-
-// or 
+// returns the mkeys and values of object car
 let car = {
     name : "class",
     manufacturer :"mercedes",
@@ -55,17 +6,7 @@ let car = {
     console.log( `${this.name} was manufactured by ${this.manufacturer}`)
     }
 }
-
-// op undefined when pointer fn used
-let car = {
-    name : "class",
-    manufacturer :"mercedes",
-    print : ()=>{
-    console.log(this) 
-    // above line in arrow fn points to the window object but in normal fn it points to its parent
-    console.log( `${this.name} was manufactured by ${this.manufacturer}`)
-    }
-}
-car.print()
-
-
+console.log(Object.values(car))
+console.log(Object.keys(car))
+// checks whether car has name in it and returns true
+console.log(car.hasOwnProperty('name'))
