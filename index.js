85187c2df22635ce1f49051bb560121e4903deb5
athira2814 <athirad193@gmail.com>
2021-10-19 20:04:42 +0530

    await

--- index.js
+++ index.js
@@ -1,20 +1,7 @@
-const table = document.querySelector('table')
-fetch('https://jsonplaceholder.typicode.com/users')
-    .then(res => res.json())
-    .then(data=>{
-        // console.log(data)
-        data.forEach(user => {
-            let newrow = document.createElement('tr')
-            // let dataID = document.createElement('td')
-            // let dataIDtextnode = document.createTextNode(user.id)
-            // dataID.appendChild(dataIDtextnode)
-            // newrow.appendChild(dataID)
-            let elem = ` 
-            <td>${user.id}</td>
-            <td>${user.name}</td>
-            <td>${user.email}</td>
-            <td>${user.username}</td>`
-            newrow.innerHTML= elem            
-            table.appendChild(newrow)
-        });
-})
+const fetchdata = async ()=>{
+    const respose = await fetch('https://jsonplaceholder.typicode.com/users')
+    console.log(respose)
+    const data = await respose.json()
+    console.log(data)
+}
+fetchdata()
