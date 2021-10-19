84366d3cf0f01c1b2c53a40808897eb15242d9de
athira2814 <athirad193@gmail.com>
2021-10-19 19:50:35 +0530

    promises

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-let Name = new Promise((resolve, reject)=>{
-    setTimeout(() => {
-        resolve('athira')
-    },3000)
+const table = document.querySelector('table')
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data=>{
+        // console.log(data)
+        data.forEach(user => {
+            let newrow = document.createElement('tr')
+            // let dataID = document.createElement('td')
+            // let dataIDtextnode = document.createTextNode(user.id)
+            // dataID.appendChild(dataIDtextnode)
+            // newrow.appendChild(dataID)
+            let elem = ` 
+            <td>${user.id}</td>
+            <td>${user.name}</td>
+            <td>${user.email}</td>
+            <td>${user.username}</td>`
+            newrow.innerHTML= elem            
+            table.appendChild(newrow)
+        });
 })
-// console.log(Name)
-// setTimeout(()=>{
-//     console.log(Name)
-// },4000)
-
-// '.then' automatically executes once the promise resolves or rejects
-
-Name.then(data=>{console.log(data)})
