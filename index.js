dcd36d13713ca3eab58dfb1604d7fa96237b6c98
athira2814 <athirad193@gmail.com>
2021-10-20 19:50:58 +0530

    destructuring

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 
-const input = document.querySelector('input')
-const button = document.querySelector('button')
+// const input = document.querySelector('input')
+// const button = document.querySelector('button')
 
-const getdata = async(item)=>{
-    console.log(item)
-    let url = `https://api.edamam.com/search?app_id=bd51454f&app_key=3b359328e30cad3141319969dfedaba9&q=${item}`
-    console.log(url)
-    const res = await fetch(url)
-    const data = await res.json()
-    console.log(data.hits)
-}
-button.addEventListener('click', (e)=>{
-    getdata(input.value)
+// const getdata = async(item)=>{
+//     console.log(item)
+//     let url = `https://api.edamam.com/search?app_id=bd51454f&app_key=3b359328e30cad3141319969dfedaba9&q=${item}`
+//     console.log(url)
+//     const res = await fetch(url)
+//     const data = await res.json()
+//     console.log(data.hits)
+// }
+// button.addEventListener('click', (e)=>{
+//     getdata(input.value)
+// })
+// object
+const person = ({
+    Name: "diya",
+    age: "20",
+    country: "India"
 })
+
+const {Name, age, country} = person 
+console.log(Name, age, country)
